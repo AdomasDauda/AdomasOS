7e31b300b69da4fdbebe2313913a35e00e7bd2aa
AdomasDauda <107749872+AdomasDauda@users.noreply.github.com>
2022-10-31 18:24:57 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,185 @@
+var mouseX, mouseY, key;
+
+window.onload = function(){
+    onmousemove = function(e){
+        mouseX = e.clientX;
+        mouseY = e.clientY;
+    }
+
+    onkeydown = function(e){
+        key = e.key;
+    }
+}
+
+document.getElementById('newTerminal').addEventListener('click', () => {
+    new Terminal();
+});
+
+
+
+
+class Terminal {
+    constructor() {
+
+
+        this.workingDirectory = '/adomasdauda/home'
+        this.isMouseOverTopBar = false;
+        this.closing = false;
+        this.isMouseOverTerminalField = false;
+        this.linecount = 0;
+
+        this.createElements();
+        this.addEventLiteners();
+
+        
+        this.commands = new Map();
+        
+        this.commands.set('>pwd', this.workingDirectory)
+        console.log(this.commands);
+    }
+
+
+    // create all of the components
+    createElements = () => {
+        this.terminal = document.createElement('div');
+        this.terminal.className = 'terminal_window';
+    
+        this.topbar = document.createElement('div');
+        this.topbar.className = 'topbar';
+        this.terminal.appendChild(this.topbar);
+
+        this.lowerbutton = document.createElement('button');
+        this.lowerbutton.className = 'topbarelement right';
+        this.lowerbutton.textContent = '_';
+        this.topbar.appendChild(this.lowerbutton);
+    
+        this.closebutton = document.createElement('button');
+        this.closebutton.className = 'topbarelement';
+        this.closebutton.textContent = 'X';
+        this.topbar.appendChild(this.closebutton);
+
+        this.terminalField = document.createElement('div');
+        this.terminalField.className = 'terminalfield';
+
+        this.createNewLine('>');
+        this.terminal.appendChild(this.terminalField);
+
+
+        document.body.appendChild(this.terminal);
+    }
+
+
+    // add event listerners to all of the above interactable components
+    addEventLiteners = () => {
+
+        this.terminalField.addEventListener('mouseover', () => {
+            this.isMouseOverTerminalField = true;
+        });
+
+        this.terminalField.addEventListener('mouseleave', () => {
+            this.isMouseOverTerminalField = false;
+        });
+
+        document.addEventListener('click', () => {
+            if (this.isMouseOverTerminalField == true){
+                this.isFieldSelected = true;
+                return;
+            }
+            this.isFieldSelected = false;
+        });
+
+        document.addEventListener('keydown', ()=> {
+            if (this.isFieldSelected == true){
+                setTimeout(() => {
+                    if (key == 'Enter'){
+                        if (!this.commands.has(this.text.textContent)) {
+                            this.createErrorLine('Invalid command. Maybe try pwd?');
+                            this.createNewLine('>');
+                            return;
+                        }
+                        this.createNewLine(this.commands.get(this.text.textContent));
+                        this.createNewLine('>');
+                        return;
+                    }
+                    
+
+                    if (key == 'Backspace'){
+                        if (this.text.textContent.length > 1){
+                            this.text.textContent = this.text.textContent.substring(0, this.text.textContent.length-1);
+                        }
+                        return; 
+                    }
+
+                    this.text.textContent = this.text.textContent + key;
+                }, 5)
+
+            }
+        });
+
+        this.closebutton.addEventListener('click', () => {
+            if (this.closing == false){ // to avoid multiple closes
+                this.closing = true;
+                this.terminal.style.width = '0%';
+                this.terminal.style.height = '0%';
+                this.terminal.style.opacity = 0.0;
+
+                this.terminal.style.left = document.getElementById('newTerminal').offsetLeft + '%';
+                this.terminal.style.top = document.getElementById('newTerminal').offsetHeight + '%';
+                setTimeout(() => {
+                    document.body.removeChild(this.terminal)
+                }, 200);
+            }
+
+        });
+
+        this.topbar.addEventListener('mouseover', () =>{
+            this.isMouseOverTopBar = true;
+        });
+        
+        this.topbar.addEventListener('mouseleave', () =>{
+            this.isMouseOverTopBar = false;
+        });
+
+        document.addEventListener('mousedown', () => {
+            if (this.isMouseOverTopBar == true){
+                this.pickUpPosX = mouseX;
+                this.pickUpPosY = mouseY;
+        
+                this.pickUpPosWindowX = this.terminal.offsetLeft;
+                this.pickUpPosWindowY = this.terminal.offsetTop;
+                
+                console.log(this.pickUpPosWindowX, this.pickUpPosWindowY);
+
+                this.loop = setInterval(() => {
+
+
+                    let dx = mouseX - this.pickUpPosX + this.pickUpPosWindowX;
+                    let dy = mouseY - this.pickUpPosY + this.pickUpPosWindowY;
+    
+                    this.terminal.style.left = dx + 'px';
+                    this.terminal.style.top = dy + 'px';
+                }, 0)
+            }
+        });
+        
+        document.addEventListener('mouseup', () => {
+            clearInterval(this.loop)
+        });
+    }
+
+    createNewLine = (text) => {
+        this.text = document.createElement('p');
+        this.text.className = 'terminaltext';
+        this.text.textContent = text;
+        this.terminalField.appendChild(this.text);
+        this.linecount++;
+    }
+
+    createErrorLine = (text) => {
+        this.text = document.createElement('p');
+        this.text.className = 'terminalerror';
+        this.text.textContent = text;
+        this.terminalField.appendChild(this.text);
+        this.linecount++;
+    }
+}
